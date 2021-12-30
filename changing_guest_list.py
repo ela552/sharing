53ed743b8d7bb10595670a99470e279b1d259837
ela552 <90276257+ela552@users.noreply.github.com>
2021-12-30 13:47:20 +0300

    Add files via upload

---
+++ changing_guest_list.py
@@ -0,0 +1,8 @@
+guest_list = ["merkel", "oprah", "kay"]
+print("Unfortunately" + " " + guest_list[2].title() + " " + "won't be able to make it"+ ".")
+
+guest_list[2] = "rice"
+print(guest_list)
+print("Hello" + " " + guest_list[0].title() + "," + "you are invited to dinner this evening at my residence" + ".")
+print("Hello" + " " + guest_list[1].title() + "," + "you are invited to dinner this evening at my residence" + ".")
+print("Hello" + " " + guest_list[2].title() + "," + "you are invited to dinner this evening at my residence" + ".")
---
+++ guest_list.py
@@ -0,0 +1,4 @@
+guest_list = ["merkel", "oprah", "kay"]
+print("Hello" + " " + guest_list[0].title() + "," + "you are invited to dinner this evening at my residence" + ".")
+print("Hello" + " " + guest_list[1].title() + "," + "you are invited to dinner this evening at my residence" + ".")
+print("Hello" + " " + guest_list[2].title() + "," + "you are invited to dinner this evening at my residence" + ".")
