e39b5ba85ba6ab6507447e453be5964d5bff3cea
ela552 <90276257+ela552@users.noreply.github.com>
2022-01-05 11:00:53 +0300

    Add files via upload

---
+++ animals.py
@@ -0,0 +1,5 @@
+animals = ["lion", "cat", "leopard", "jaguar", "tiger"]
+for animal in animals:
+    print(animal.title())
+    print("A" + " " + animal.title() + " " + "would make such a nice pet.")
+print("\nAll these animals belong to the cat family!")
